940d231e0e65b9b7690722730015feb420735947
Ayslan-gamedev <ayslan.gamedev@gmail.com>
2023-08-21 22:18:05 -0300

    STYLE - Player Atributts in struct

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Mathematics;
+using TMPro;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
@@ -7,10 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D RigidBody2d;
 
-    public float speed;
-
-    public float aceletarion;
-    public float slowdown;
+    [SerializeField] private PlayerAtributts playerAtributts;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +29,9 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 arrowsDirections = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        currentSpeed = arrowsDirections != Vector2.zero ? currentSpeed += aceletarion : currentSpeed -= slowdown;
+        currentSpeed = arrowsDirections != Vector2.zero ? currentSpeed += playerAtributts.aceletarion : currentSpeed -= playerAtributts.slowdown;
 
-        currentSpeed = math.clamp(currentSpeed, 0, speed);
+        currentSpeed = math.clamp(currentSpeed, 0, playerAtributts.speed);
 
         if(arrowsDirections != Vector2.zero)
         {
@@ -42,4 +40,11 @@ public class PlayerMovement : MonoBehaviour
 
         RigidBody2d.velocity = persistentDirection * currentSpeed;
     }
+}
+
+[System.Serializable]
+public struct PlayerAtributts {
+    public float speed;
+    public float aceletarion;
+    public float slowdown;
 }
